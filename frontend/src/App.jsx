928bb17d0efa065d1d2a46b89c653db92b5a2ba2
abdiranman <abdirahman.abdi330@.com>
2023-11-21 08:45:45 -0500

    Display 3 different photots

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
@@ -14,13 +13,16 @@ export const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-// Note: Rendering a single component to build components in isolation
-const App = () => {
+function App() {
+  const photos = new Array(3).fill(sampleDataForPhotoListItem);
+
   return (
     <div className="App">
-       <PhotoListItem {...sampleDataForPhotoListItem} />
+      {photos.map((photo, index) => (
+        <PhotoListItem key={index} {...photo} />
+      ))}
     </div>
   );
-};
+}
 
 export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
-
-const PhotoListItem = ({ username, imageSource, id, location, profile }) => {
+const PhotoListItem = (props) => {
   return (
-    <div key={id}>
-      <img src={imageSource} alt="photo" />
-      <h2>{username}</h2>
-      <p>{location.city}, {location.country}</p>
-      <img src={profile} alt="profile" />
+    <div className="photo-list__item">
+      <img
+        className="photo-list__image"
+        src={props.imageSource}
+        alt={`Photo ${props.id}`}
+      />
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={props.profile}
+          alt={`${props.username}'s profile`}
+        />
+      </div>
+      <span className="photo-list__user-info">{props.username}</span>
+      <div className="photo-list__user-location">{props.location.city}</div>
+      <div className="photo-list__user-location">{props.location.country}</div>
     </div>
   );
 };
-
-
-
-
 export default PhotoListItem;
