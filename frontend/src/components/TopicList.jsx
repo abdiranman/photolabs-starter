70bc85b3094a937159a6d51f0ec447295c1b7718
abdiranman <abdirahman.abdi330@.com>
2023-11-21 13:10:05 -0500

    Photos are loaded from API based on catagory

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
 const TopicList = (props) => {
   return (
     <div className="top-nav-bar__topic-list">
-      <TopicListItem TopicData={props.topics} />
+      <TopicListItem
+        TopicData={props.topics}
+        fetchPhotosByTopic={props.fetchPhotosByTopic} // pass the function to component
+      />
     </div>
   );
 };
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "../styles/TopicListItem.scss";
-
 const TopicListItem = (props) => {
   const topics = props.TopicData;
 
+  const topicHandler = (id) => {
+    props.fetchPhotosByTopic(id); // function with the topic id
+  };
+
   const topicItem = topics.map((topic) => (
-    <div className="topic-list__item" key={topic.id}>
+    <div
+      className="topic-list__item"
+      key={topic.id}
+      onClick={() => topicHandler(topic.id)} // Pass the topic ID
+    >
       <span>{topic.title}</span>
     </div>
   ));
   return <div>{topicItem}</div>;
 };
-
 export default TopicListItem;
